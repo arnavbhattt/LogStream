8f82a0918e68082dd2d92533fef5c2e4ebc323bd
arnavbhattt <arnxv23@gmail.com>
2024-10-30 18:36:25 -0400

    finished impl of segment

--- go.mod
+++ go.mod
@@ -2,11 +2,15 @@ module github.com/arnavbhattt/LogStream
 
 go 1.23.2
 
-require github.com/stretchr/testify v1.9.0
+require (
+	github.com/arnavbhattt/protobuf-go v0.0.0-20241027191232-0a67e0192784
+	github.com/stretchr/testify v1.9.0
+	github.com/tysonmote/gommap v0.0.3
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/tysonmote/gommap v0.0.3 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/log/segment.go
+++ internal/log/segment.go
@@ -3,7 +3,11 @@ package log
 import (
 	"fmt"
 	"os"
+
 	"path"
+
+	api "github.com/arnavbhattt/protobuf-go/api/v1"
+	"google.golang.org/protobuf/proto"
 )
 
 /*
@@ -53,4 +57,77 @@ func newSegment(dir string, baseOffset uint64, c Config) (*segment, error) {
 		s.nextOffset = baseOffset + uint64(off) + 1
 	}
 	return s, nil
+}
+
+func (s *segment) Append(record *api.Record) (offset uint64, err error) {
+	cur := s.nextOffset
+	record.Offset = cur
+	p, err := proto.Marshal(record)
+	if err != nil {
+		return 0, err
+	}
+	_, pos, err := s.store.Append(p)
+	if err != nil {
+		return 0, nil
+	}
+	if err = s.index.Write(
+		uint32(s.nextOffset-uint64(s.baseOffset)),
+		pos,
+	); err != nil {
+		return 0, err
+	}
+	s.nextOffset++
+	return cur, nil
+}
+
+func (s *segment) Read(off uint64) (*api.Record, error) {
+	_, pos, err := s.index.Read(int64(off - s.baseOffset))
+	if err != nil {
+		return nil, err
+	}
+	p, err := s.store.Read(pos)
+	if err != nil {
+		return nil, err
+	}
+	record := &api.Record{}
+	err = proto.Unmarshal(p, record)
+	return record, err
+}
+
+/*
+Check if segment has reached max size
+*/
+func (s *segment) isMaxed() bool {
+	return s.store.size >= s.config.Segment.MaxStoreBytes ||
+		s.index.size >= s.config.Segment.MaxIndexBytes
+}
+
+func (s *segment) Close() error {
+	if err := s.index.Close(); err != nil {
+		return err
+	}
+	if err := s.store.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *segment) Remove() error {
+	if err := s.Close(); err != nil {
+		return err
+	}
+	if err := os.Remove(s.index.Name()); err != nil {
+		return err
+	}
+	if err := os.Remove(s.store.Name()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func nearestMultiple(j, k uint64) uint64 {
+	if j >= 0 {
+		return (j / k) * k
+	}
+	return ((j - k + 1) / k) * k
 }
